29714978364ae5514b34969d409af68f22c68970
Ibodullayev-Hasan <ibodullayevh040@gmail.com>
2025-03-26 18:26:42 +0500

    uploads static papkasi main.ts da ro'yxatga olindi

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CorsConfig, SetupGlobalPipes, SwaggerConfig } from './configs';
 import { NotFoundExceptionFilter } from './filters/http-exception.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configServie = app.get(ConfigService)
 
   app.setGlobalPrefix('api')
@@ -14,9 +16,10 @@ async function bootstrap() {
   SwaggerConfig(app)
   SetupGlobalPipes(app)
 
+
   app.useGlobalFilters(new NotFoundExceptionFilter())
 
-  const port = configServie.get<number>("PORT")
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), { prefix: '/uploads' }); const port = configServie.get<number>("PORT")
 
   await app.listen(port, () => {
     console.log(`Server run on port: ${port}`)
